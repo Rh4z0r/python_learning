0c3602906f7b60ea0193a2f19c6047fac052e07c
Rh4z0r <123068227+Rh4z0r@users.noreply.github.com>
2026-07-02 18:31:50 +0000

    Uebungsblatt 4 Aufgabe 3

--- Uebungsblatt 4/Aufgabe 3.py
+++ Uebungsblatt 4/Aufgabe 3.py
@@ -1,9 +1,9 @@
 # Erweitern Sie den Code zu geometrischen Formen des ersten
 # Ubungsblattes. In dieser Aufgabe
-# sollten Sie die Fl¨ ache von Grundst¨ ucken berechnen und auf dieser Basis den Brutto- und Netto-
-# preis ausgeben. Stellen Sie sich nun vor, dass dieses Programm in einer Stadtverwaltung f¨ ur viele
-# verschiedene Grundst¨ ucke eingesetzt werden soll. Erweitern Sie also Ihr Programm dahingehend,
-# dass die L¨ ange und Breite von einzelnen Grundst¨ ucke in einer Liste verwaltet werden. Lagern Sie
+# sollten Sie die Fläche von Grundstücken berechnen und auf dieser Basis den Brutto- und Netto-     
+# preis ausgeben. Stellen Sie sich nun vor, dass dieses Programm in einer Stadtverwaltung für viele
+# verschiedene Grundstücke eingesetzt werden soll. Erweitern Sie also Ihr Programm dahingehend,
+# dass die Länge und Breite von einzelnen Grundstücken in einer Liste verwaltet werden. Lagern Sie
 # außerdem die Berechnung der Netto- und Bruttopreise in Funktionen aus. Achten Sie hierbei auf
 # sinnvolle Funktionssignaturen mit eventuellen Standardwerten der Parameter. Geben Sie dann mit
 # Hilfe einer for-Schleife und den geschriebenen Funktionen den Brutto- und Nettopreis f¨ ur jedes
